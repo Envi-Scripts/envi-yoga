2d5c2b3e3594a3e8803cd7aa8015dfce8c75a400
Envi-Scripts <126303539+Envi-Scripts@users.noreply.github.com>
2023-02-28 21:29:21 +0000

    v1.1
    code refactor

--- envi-yoga/server/server.lua
+++ envi-yoga/server/server.lua
@@ -1,81 +1,40 @@
-if Config.Framework == 'esx' then	
-	ESX = nil
-	TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+if Config.Framework == 'esx' then    
+    ESX = nil
+    TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-	ESX.RegisterUsableItem('yogamat_pro',function(source)
-		local xPlayer = ESX.GetPlayerFromId(source)
-		TriggerClientEvent('envi-yoga:mat',source)
-		xPlayer.removeInventoryItem('yogamat_pro',1)
-	end)
-
-	ESX.RegisterUsableItem('yogamat_blue',function(source)
-		local xPlayer = ESX.GetPlayerFromId(source)
-		TriggerClientEvent('envi-yoga:mat1',source)
-		xPlayer.removeInventoryItem('yogamat_blue',1)
-	end)
-
-	ESX.RegisterUsableItem('yogamat_black',function(source)
-		local xPlayer = ESX.GetPlayerFromId(source)
-		TriggerClientEvent('envi-yoga:mat2',source)
-		xPlayer.removeInventoryItem('yogamat_black',1)
-	end)
-
-	ESX.RegisterUsableItem('yogamat_red',function(source)
-		local xPlayer = ESX.GetPlayerFromId(source)
-		TriggerClientEvent('envi-yoga:mat3',source)
-		xPlayer.removeInventoryItem('yogamat_red',1)
-	end)
-
-	RegisterNetEvent('envi-yoga:pickupS',function(variation)
-		local xPlayer = ESX.GetPlayerFromId(source)
-		if variation == 876225403 then
-			xPlayer.addInventoryItem('yogamat_pro',1)
-		elseif variation == -232023078 then
-			xPlayer.addInventoryItem('yogamat_blue',1)
-		elseif variation == 2057317573 then
-			xPlayer.addInventoryItem('yogamat_black',1)
-		elseif variation == -1978741854 then
-			xPlayer.addInventoryItem('yogamat_red',1)
-		end
-	end)
+    for k, v in pairs(Config.YogaMats) do
+        ESX.RegisterUsableItem(k, function(source)
+            local xPlayer = ESX.GetPlayerFromId(source)
+            TriggerClientEvent('envi-yoga:placemat', source, k, v)
+            xPlayer.removeInventoryItem(k, 1)
+        end)
+    end
 elseif Config.Framework == 'qb' then
-	local QBCore = exports['qb-core']:GetCoreObject()
-	QBCore.Functions.CreateUseableItem('yogamat_pro',function(source)
-		local Player = QBCore.Functions.GetPlayer(source)
-		TriggerClientEvent('envi-yoga:mat',source)
-		Player.Functions.RemoveItem('yogamat_pro',1)
-	end)
-
-	QBCore.Functions.CreateUseableItem('yogamat_blue',function(source)
-		local Player = QBCore.Functions.GetPlayer(source)
-		TriggerClientEvent('envi-yoga:mat1',source)
-		Player.Functions.RemoveItem('yogamat_blue',1)
-	end)
-
-	QBCore.Functions.CreateUseableItem('yogamat_black',function(source)
-		local Player = QBCore.Functions.GetPlayer(source)
-		TriggerClientEvent('envi-yoga:mat2',source)
-		Player.Functions.RemoveItem('yogamat_black',1)
-	end)
-
-	QBCore.Functions.CreateUseableItem('yogamat_red',function(source)
-		local Player = QBCore.Functions.GetPlayer(source)
-		TriggerClientEvent('envi-yoga:mat3',source)
-		Player.Functions.RemoveItem('yogamat_red',1)
-	end)
-
-	RegisterNetEvent('envi-yoga:pickupS',function(variation)
-		local Player = QBCore.Functions.GetPlayer(source)
-		if variation == 876225403 then
-			Player.Functions.AddItem('yogamat_pro',1)
-		elseif variation == -232023078 then
-			Player.Functions.AddItem('yogamat_blue',1)
-		elseif variation == 2057317573 then
-			Player.Functions.AddItem('yogamat_black',1)
-		elseif variation == -1978741854 then
-			Player.Functions.AddItem('yogamat_red',1)
-		end
-	end)
+    local QBCore = exports['qb-core']:GetCoreObject()
+    for k, v in pairs(Config.YogaMats) do
+        QBCore.Functions.CreateUseableItem(k, function(source, item)
+            local Player = QBCore.Functions.GetPlayer(source)
+            TriggerClientEvent('envi-yoga:placemat', source, item, v)
+            Player.Functions.RemoveItem(item, 1)
+        end)
+    end
 end
 
+local function GetItemFromModel(model)
+    for k, v in pairs(Config.YogaMats) do
+        if model == v then
+            return k
+        end
+    end
+end
 
+RegisterNetEvent('envi-yoga:pickup', function(model)
+    local item = GetItemFromModel(model)
+    if Config.Framework == 'esx' then
+        local xPlayer = ESX.GetPlayerFromId(source)
+        xPlayer.addInventoryItem(item, 1)
+    elseif Config.Framework == 'qb' then
+        local Player = QBCore.Functions.GetPlayer(source)
+        Player.Functions.AddItem(item, 1)
+    end
+end)
