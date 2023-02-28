938ac54632a118da1a03bb1ab9eaa77f93a86216
Envi-Scripts <126303539+Envi-Scripts@users.noreply.github.com>
2023-02-28 18:53:20 +0000

    Add files via upload

---
+++ envi-yoga/[INSTALL]/items for ox.lua
@@ -0,0 +1,28 @@
+['yogamat_pro'] = {
+	label = 'ProLaps Yoga Mat',
+	weight = 500,
+	stack = true,
+	close = true,
+	description = 'A Limited Edition ProLaps Yogamat.'
+},
+['yogamat_blue'] = {
+	label = 'Blue Yoga Mat',
+	weight = 500,
+	stack = true,
+	close = true,
+	description = 'Did somebody say Yoga?',
+},
+['yogamat_black'] = {
+	label = 'Black Yoga Mat',
+	weight = 500,
+	stack = true,
+	close = true,
+	description = 'Did somebody say Yoga?',
+},
+['yogamat_red'] = {
+	label = 'Red Yoga Mat',
+	weight = 500,
+	stack = true,
+	close = true,
+	description = 'Did somebody say Yoga?',
+},
