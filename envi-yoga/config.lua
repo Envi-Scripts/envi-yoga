7c22e3807e5bb82ab5b326591a68600efb37e215
Envi-Scripts <126303539+Envi-Scripts@users.noreply.github.com>
2023-02-28 21:12:25 +0000

    v1.1
    code refactoring

--- envi-yoga/config.lua
+++ envi-yoga/config.lua
@@ -3,3 +3,10 @@ Config = {}
 Config.Framework = 'esx' -- 'esx' or 'qb'
 
 Config.Target = 'ox_target' -- 'qtarget' or 'ox_target' or 'qb-target'
+
+Config.YogaMats = {
+    ['yogamat_pro'] = `p_yoga_mat_01_s`,
+    ['yogamat_blue'] = `prop_yoga_mat_01`,
+    ['yogamat_black'] = `prop_yoga_mat_02`,
+    ['yogamat_red'] = `prop_yoga_mat_03`
+}
